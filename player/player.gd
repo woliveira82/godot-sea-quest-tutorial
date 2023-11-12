bfb4d7ff91158c832bd42a57626bee70a8cdf3b4
Weiner Oliveira <woliveira82@gmail.com>
2023-11-12 18:18:05 +0000

    3 - Player Movement

---
+++ player/player.gd
@@ -0,0 +1,15 @@
+extends AnimatedSprite2D
+
+var velocity = Vector2.ZERO
+var SPEED = Vector2(125, 90)
+
+
+func _process(delta):
+	velocity.x = Input.get_axis("move_left", "move_right")
+	velocity.y = Input.get_axis("move_up", "move_down")
+	
+	velocity = velocity.normalized()
+
+
+func _physics_process(delta):
+	global_position += velocity * SPEED * delta
