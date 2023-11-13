1b38f5248a89500d802e0bfc822d2d2a3dc28279
Weiner Oliveira <woliveira82@gmail.com>
2023-11-13 22:35:22 +0000

    feat: bullet face, lifetime and direction

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,13 @@
 extends AnimatedSprite2D
 
 var velocity = Vector2.ZERO
-var SPEED = Vector2(125, 90)
+var can_shoot = true
+
+const SPEED = Vector2(125, 90)
+const BULLET_OFFSET = 7
 const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
+
+@onready var reload_timer = $ReloadTimer
 
 
 func _process(delta):
@@ -16,10 +21,24 @@ func _process(delta):
 	elif velocity.x < 0:
 		flip_h = true
 
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
 		bullet_instance.global_position = global_position
 		get_tree().current_scene.add_child(bullet_instance)
+		
+		if flip_h:
+			bullet_instance.flip_direction()
+			bullet_instance.global_position = global_position - Vector2(BULLET_OFFSET, 0)
+		else:
+			bullet_instance.global_position = global_position + Vector2(BULLET_OFFSET, 0)
+		
+		reload_timer.start()
+		can_shoot = false
+
 
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta
+
+
+func _on_reload_timer_timeout():
+	can_shoot = true
--- player/player_bullet/player_bullet.gd
+++ player/player_bullet/player_bullet.gd
@@ -5,5 +5,19 @@ var velocity = Vector2.RIGHT
 const SPEED = 300
 
 
+func flip_direction():
+	velocity = -velocity
+	flip_h = not flip_h
+
+
+func _ready():
+	rotation_degrees = randf_range(-7.0, 7.0)
+	velocity = velocity.rotated(rotation)
+
+
 func _physics_process(delta):
 	global_position += velocity * SPEED * delta 
+
+
+func _screen_exited():
+	queue_free()
