5afdb8a2d458e2b75b17ca16e52f4caf9441243d
Weiner Oliveira <woliveira82@gmail.com>
2023-11-18 11:20:57 +0000

    feat: oxygen level and player refact

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,7 @@ var velocity = Vector2.ZERO
 var can_shoot = true
 
 const SPEED = Vector2(125, 90)
+const OXYGEN_DECREASE_SPEED = 2.5
 const BULLET_OFFSET = 7
 const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
 
@@ -12,16 +13,26 @@ const Bullet = preload("res://player/player_bullet/player_bullet.tscn")
 
 
 func _process(_delta):
+	process_movement_input()
+	direction_follows_input()
+	process_shooting()
+	lose_oxygen()
+
+
+func process_movement_input():
 	velocity.x = Input.get_axis("move_left", "move_right")
 	velocity.y = Input.get_axis("move_up", "move_down")
-	
 	velocity = velocity.normalized()
-	
+
+
+func direction_follows_input():
 	if velocity.x > 0:
 		sprite.flip_h = false
 	elif velocity.x < 0:
 		sprite.flip_h = true
 
+
+func process_shooting():
 	if Input.is_action_pressed("shoot") and can_shoot:
 		var bullet_instance = Bullet.instantiate()
 		bullet_instance.global_position = global_position
@@ -37,8 +48,16 @@ func _process(_delta):
 		can_shoot = false
 
 
-func _physics_process(delta):
-	global_position += velocity * SPEED * delta
+func lose_oxygen():
+	Global.oxygen_level -= OXYGEN_DECREASE_SPEED * get_process_delta_time()
+
+
+func movement():
+	global_position += velocity * SPEED * get_physics_process_delta_time()
+
+
+func _physics_process(_delta):
+	movement()
 
 
 func _on_reload_timer_timeout():
