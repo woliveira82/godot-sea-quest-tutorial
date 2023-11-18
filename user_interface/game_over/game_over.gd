5919300d8882553f3b0526c38e74f8d035b5368f
Weiner Oliveira <woliveira82@gmail.com>
2023-11-18 22:54:31 +0000

    feat: add game over screen

---
+++ user_interface/game_over/game_over.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+@onready var current_scene_label = $CurrentScoreLabel
+@onready var game_over_delay = $GameOverDelay
+
+func _ready():
+	GameEvents.connect("game_over", Callable(self, "_activate_game_over"))
+	visible = false
+
+
+func _process(delta):
+	if visible == true and Input.is_action_just_pressed("shoot"):
+		Global.current_points = 0
+		Global.saved_people_count = 0
+		Global.oxygen_level = 100
+		get_tree().reload_current_scene()
+
+
+func _activate_game_over():
+	current_scene_label.text = "Score %d" % Global.current_points
+	game_over_delay.start()
+
+
+func _on_game_over_delay_timeout():
+	visible = true
